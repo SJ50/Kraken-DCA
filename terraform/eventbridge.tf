e6eb26e1dfeaa0eab1b21384527b1be22c5f0584
Sameer Jain <sameerjain.50@gmail.com>
2024-11-07 19:29:12 +1100

    fix trading pair

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_event_target" "kraken_dca_lambda_event_target" {
   rule  = aws_cloudwatch_event_rule.kraken_dca_lambda_event_rule.name
   input = <<EOF
     {
-        "trading_pair": "XXBTZEUR",
+        "trading_pair": "XXBTZAUD",
         "crypto_to_buy": "BTC"
     }
     EOF
